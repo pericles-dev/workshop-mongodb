aea39ad4e447e4a2d37538720b177a47e317e77d
Pericles Santos <pericles.programador@gmail.com>
2024-08-26 16:12:59 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.example.demo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.demo.entities.User;
+import com.example.demo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User pericles = new User(null, "Pericles Santos", "pericles@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob, pericles));
+		
+	}
+
+}
