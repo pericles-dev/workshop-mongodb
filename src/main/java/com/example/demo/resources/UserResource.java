51ff5a6dc81935844707494e22c0a0f927998a8d
Pericles Santos <pericles.programador@gmail.com>
2024-08-26 18:04:41 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,7 @@ public class UserResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Void> inserr(@RequestBody UserDto objDto) 	{
+	public ResponseEntity<Void> insert(@RequestBody UserDto objDto) 	{
 		
 		User obj = service.fromDto(objDto);
 		obj = service.insert(obj);
@@ -58,6 +59,16 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable String id) 	{
 		
 		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDto objDto, @PathVariable String id) 	{
+		
+		User obj = service.fromDto(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
 		
 		return ResponseEntity.noContent().build();
 	}
