7a3931c655f354c7ceaf21bea3cf4748cde7372d
Pericles Santos <pericles.programador@gmail.com>
2024-08-27 06:01:22 -0300

    Consulta com varios criterios

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,11 +10,13 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.entities.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
-	
+public interface PostRepository extends MongoRepository<Post, String> {
+
 	@Query("{'title' : {$regex: ?0, $options: 'i'}}")
 	List<Post> searchByTitle(String text);
-	
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
